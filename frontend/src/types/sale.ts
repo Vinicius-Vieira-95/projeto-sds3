b823fb0e38a23a86a0cd417859fed0bb307c3449
Vinicius Vieira <vinicius.95vieira00@gmail.com>
2021-05-07 14:27:56 -0300

    Data table integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,4 @@
-import { type } from "node:os"
+import {Seller} from './seller'
 
 export type SaleSum =  {
     sellerName : string,
@@ -9,4 +9,25 @@ export type SaleSuccess =  {
     sellerName : string,
     visited    : number,
     deals      : number
+}
+
+export type Sale = {
+    id      : number,
+    visited : number,
+    deals   : number,
+    amount  : number,
+    date    : string
+    seller  : Seller
+}
+
+export type SalePage = {
+    content?      : Sale[],
+    last          : boolean,
+    totalElements : number,
+    totalPAge     : number,
+    size?         : number,
+    number        : number,
+    first         : boolean,
+    numberOfElements?: number
+    empty?        : boolean
 }
